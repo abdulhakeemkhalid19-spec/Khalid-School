fe47e49cd4769e72ff570a362c8ba02d33677527
abdulhakeemkhalid19-spec <abdulhakeemkhalid19@gmail.com>
2026-05-22 07:29:46 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 import { AuthProvider, useAuth } from './contexts/AuthContext'
+import { ThemeProvider } from './lib/theme'
 import Login from './pages/Login'
 import AdminDashboard from './pages/admin/Dashboard'
 import TeacherDashboard from './pages/teacher/Dashboard'
@@ -61,13 +62,15 @@ function AppRoutes() {
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AuthProvider>
-          <AppRoutes />
-          <Toaster richColors position="top-right" />
-        </AuthProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <HashRouter>
+          <AuthProvider>
+            <AppRoutes />
+            <Toaster richColors position="top-right" />
+          </AuthProvider>
+        </HashRouter>
+      </QueryClientProvider>
+    </ThemeProvider>
   )
 }
